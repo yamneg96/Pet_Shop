b7530732192f70e996c89ab599277798c6a1ee20
Yamlak Negash <yamlaknegash96@gmail.com>
2026-01-01 23:19:34 +0300

    Added Projects and Skills

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com"]
-  }
-  // },
-  // experimental: {
-  //   serverActions: true
-  // }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
